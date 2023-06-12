0b64ea888f17db2a421f8d44b3dea7428b449938
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-06-12 09:20:34 +0800

    Add main container div

---
+++ script.js
@@ -0,0 +1,9 @@
+const mainContainer = document.createElement('div');
+mainContainer.id = "mainContainer";
+// Make sure that EVERY divs are inside of this block to make sure they are in one container.
+
+
+
+
+
+document.body.appendChild(mainContainer);
