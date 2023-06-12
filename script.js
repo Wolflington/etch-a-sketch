b77125aef5fcebcee9faefe5c6dc710667950dc6
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-06-12 14:53:40 +0800

    Add class for a singular square grid

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ mainContainer.id = "mainContainer";
 // Make sure that EVERY divs are inside of this block to make sure they are in one container.
     const gridContainer = document.createElement('div');
     gridContainer.classList.add('gridContainer');
-    mainContainer.appendChild(gridContainer);
-    
+        
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        gridContainer.appendChild(grid);
+
+    mainContainer.appendChild(gridContainer); 
+
 document.body.appendChild(mainContainer);
