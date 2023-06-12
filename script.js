c6fc2108fa2037df1d22ef03092e6cd580bed7fa
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-06-12 10:12:33 +0800

    Append gridContainer to mainContainer

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const mainContainer = document.createElement('div');
 mainContainer.id = "mainContainer";
 // Make sure that EVERY divs are inside of this block to make sure they are in one container.
-
-
-
-
+    const gridContainer = document.createElement('div');
+    gridContainer.classList.add('gridContainer');
+    mainContainer.appendChild(gridContainer);
 
 document.body.appendChild(mainContainer);
