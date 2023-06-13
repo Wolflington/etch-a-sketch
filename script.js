f3bf9e3c4cbf70288c56b49f5fde919be7ee66d0
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-06-13 09:53:57 +0800

    Loop the grid to specify how many grids to make in the container

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ mainContainer.id = "mainContainer";
 // Make sure that EVERY divs are inside of this block to make sure they are in one container.
     const gridContainer = document.createElement('div');
     gridContainer.classList.add('gridContainer');
-        
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
-        gridContainer.appendChild(grid);
+
+        for (i = 0; i < 40; i++) {
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            gridContainer.appendChild(grid);
+        }
 
     mainContainer.appendChild(gridContainer); 
 
