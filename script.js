df614e6762c407b5770c6351a7b39e90f2e1c06d
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-06-15 13:39:31 +0800

    Add three event listeners to turn the grids black

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ mainContainer.id = "mainContainer";
     mainContainer.appendChild(gridContainer); 
 
 document.body.appendChild(mainContainer);
+
+const gridButtons = document.querySelectorAll('.grid');
+// gridButtons.forEach(btn => {
+//     btn.addEventListener('mousedown', () => {
+//         console.log('I am clicked!');
+//         btn.style.backgroundColor = 'black';
+//     });
+// });
+gridButtons.forEach(fcn => {
+    fcn.onmousedown = () => eventHandler(event);
+    fcn.onmouseover = () => eventHandler(event);
+    fcn.onmouseup = () => eventHandler(event);
+});
+
+
+function eventHandler(event) {
+    if (event.type === 'mousedown'){
+        console.log('I am clicked!')
+        event.currentTarget.style.backgroundColor = 'black';
+   } else if (event.type === 'mouseover') {
+        console.log('I am hovered!');
+        event.currentTarget.style.backgroundColor = 'black';
+   } else if (event.type === 'mouseup') {
+        console.log('I am free!');
+        event.currentTarget.style.backgroundColor = 'white';
+   }
+}
