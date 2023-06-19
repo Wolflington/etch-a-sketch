1af08ac679ce341c4b6cc6b8ece79c98d095b383
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-06-19 09:48:52 +0800

    Make a function that creates 16x16 grids

--- script.js
+++ script.js
@@ -1,42 +1,10 @@
-const mainContainer = document.createElement('div');
-mainContainer.id = "mainContainer";
-// Make sure that EVERY divs are inside of this block to make sure they are in one container.
-    const gridContainer = document.createElement('div');
-    gridContainer.classList.add('gridContainer');
-
-        for (i = 0; i < 256; i++) {
-            const grid = document.createElement('div');
-            grid.classList.add('grid');
-            gridContainer.appendChild(grid);
-        }
-
-    mainContainer.appendChild(gridContainer); 
-
-document.body.appendChild(mainContainer);
-
-const gridButtons = document.querySelectorAll('.grid');
-// gridButtons.forEach(btn => {
-//     btn.addEventListener('mousedown', () => {
-//         console.log('I am clicked!');
-//         btn.style.backgroundColor = 'black';
-//     });
-// });
-gridButtons.forEach(fcn => {
-    fcn.onmousedown = () => eventHandler(event);
-    fcn.onmouseover = () => eventHandler(event);
-    fcn.onmouseup = () => eventHandler(event);
-});
-
-
-function eventHandler(event) {
-    if (event.type === 'mousedown'){
-        console.log('I am clicked!')
-        event.currentTarget.style.backgroundColor = 'black';
-   } else if (event.type === 'mouseover') {
-        console.log('I am hovered!');
-        event.currentTarget.style.backgroundColor = 'black';
-   } else if (event.type === 'mouseup') {
-        console.log('I am free!');
-        event.currentTarget.style.backgroundColor = 'white';
-   }
+const gridContainer = document.querySelector('.grid-container');
+function createGrids(size) {
+    size = 16 * 16;
+    for (i = 0; i < size; i++) {
+        const grids = document.createElement('div');
+        grids.classList.add('grids');
+        gridContainer.appendChild(grids);
+    }
 }
+createGrids();
