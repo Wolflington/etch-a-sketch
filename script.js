f22b0ccec60fdb8a593e76acafa26bd860a22b12
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-06-21 11:21:42 +0800

    Set conditions when the draw is enabled/disabled. Attach the conditions to listeners

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const gridContainer = document.querySelector('.grid-container');
-function createGrids(size) {
-    size = 16 * 16;
-    for (i = 0; i < size; i++) {
+let gridSize = 16*16;
+
+function createGrids() {
+    for (i = 0; i < gridSize; i++) {
         const grids = document.createElement('div');
         grids.classList.add('grids');
         gridContainer.appendChild(grids);
     }
 }
 createGrids();
+
+let mouseDown = false;
+const enableDraw = () => mouseDown = true;
+const disableDraw = () => mouseDown = false;
+
+
+document.onmousedown = enableDraw;
+document.onmouseup = disableDraw;
