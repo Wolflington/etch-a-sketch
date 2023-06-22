e668d5d438b5395d0601bab21b5cc945860f2037
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-06-22 09:52:05 +0800

    Create functions that draws in the grids when clicked and hovered

--- script.js
+++ script.js
@@ -5,15 +5,32 @@ function createGrids() {
     for (i = 0; i < gridSize; i++) {
         const grids = document.createElement('div');
         grids.classList.add('grids');
+        grids.setAttribute('draggable', false);
         gridContainer.appendChild(grids);
     }
 }
 createGrids();
 
 let mouseDown = false;
-const enableDraw = () => mouseDown = true;
-const disableDraw = () => mouseDown = false;
+function enableDraw(e) {
+    mouseDown = true;
+    e.currentTarget.style.backgroundColor = 'black';
+}
 
+function disableDraw() {
+    mouseDown = false;
+}
 
-document.onmousedown = enableDraw;
-document.onmouseup = disableDraw;
+function drawGrids(e) {
+    if (mouseDown) {
+        e.currentTarget.style.backgroundColor = 'black';
+        console.log(e);
+    }
+}
+
+let grids = document.querySelectorAll('.grids');
+grids.forEach(fcn => {
+    fcn.addEventListener('mousedown', enableDraw);
+    fcn.addEventListener('mouseup', disableDraw);
+    fcn.addEventListener('mouseover', drawGrids);
+});
