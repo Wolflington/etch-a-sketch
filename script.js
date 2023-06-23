5ef04de4502e8096b09784633414deb3a9114242
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-06-23 10:21:51 +0800

    Create an event listener that is attached to the slider to specify the number of grids

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const gridContainer = document.querySelector('.grid-container');
 let gridSize = 16*16;
 
+
 function createGrids() {
     for (i = 0; i < gridSize; i++) {
         const grids = document.createElement('div');
@@ -10,6 +11,13 @@ function createGrids() {
     }
 }
 createGrids();
+
+let grids = document.querySelectorAll('.grids');
+grids.forEach(fcn => {
+    fcn.addEventListener('mousedown', enableDraw);
+    fcn.addEventListener('mouseup', disableDraw);
+    fcn.addEventListener('mouseover', drawGrids);
+});
 
 let mouseDown = false;
 function enableDraw(e) {
@@ -28,9 +36,11 @@ function drawGrids(e) {
     }
 }
 
-let grids = document.querySelectorAll('.grids');
-grids.forEach(fcn => {
-    fcn.addEventListener('mousedown', enableDraw);
-    fcn.addEventListener('mouseup', disableDraw);
-    fcn.addEventListener('mouseover', drawGrids);
-});
+const slider = document.querySelector('.slider');
+let output = document.querySelector('.grid-size');
+output.innerText = `Grid Size: ${slider.value} x ${slider.value}`;
+slider.oninput = () => getSliderValue();
+
+function getSliderValue () {
+    output.innerText = `Grid Size: ${slider.value} x ${slider.value}`;
+}
