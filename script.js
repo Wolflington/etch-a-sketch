9d310c3ea6dfcc678eac59ebf981805bfb464120
Joseph Ely Tuyor <ely.tuyor@yahoo.com>
2023-06-27 09:08:16 +0800

    Assign template columns and rows to grid container

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-const gridContainer = document.querySelector('.grid-container');
+
 const slider = document.querySelector('.slider');
 let output = document.querySelector('.grid-size');
-output.innerText = `Grid Size: ${slider.value} x ${slider.value}`;
-slider.oninput = () => getSliderValue();
 let gridSize;
 
+slider.oninput = () => getSliderValue();
+output.innerText = `Grid Size: ${slider.value} x ${slider.value}`; //Shows the initial value, which is 16
+
+//Change the value of the slider when dragged
 function getSliderValue () {
     gridSize = slider.value;
     output.innerText = `Grid Size: ${slider.value} x ${slider.value}`;
     console.log(gridSize);
 }
 
-
+// Use loop to create the grids
 function createGrids() {
-    let gridSize = slider.value*slider.value;
+    //Get the value, including the oninput value, of slider to specify the number of grids
+    gridSize = slider.value; //Just gets the initial value set in HTML instead of the new value when the slider is dragged
     console.log(gridSize);
-    for (i = 0; i < gridSize; i++) {
+
+    //Add style to the grid container
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr);`
+
+    for (i = 0; i < gridSize*gridSize; i++) {
         const grids = document.createElement('div');
         grids.classList.add('grids');
         grids.setAttribute('draggable', false);
@@ -24,6 +33,7 @@ function createGrids() {
 }
 createGrids();
 
+//forEach loop the grids since querySelectorAll returns a NodeList
 let grids = document.querySelectorAll('.grids');
 grids.forEach(fcn => {
     fcn.addEventListener('mousedown', enableDraw);
@@ -31,6 +41,7 @@ grids.forEach(fcn => {
     fcn.addEventListener('mouseover', drawGrids);
 });
 
+//Create a variable that "turns the switch on/off"  when the mouse button is clicked
 let mouseDown = false;
 function enableDraw(e) {
     mouseDown = true;
